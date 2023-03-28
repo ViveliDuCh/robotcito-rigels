ba3fe3cd5468b8065bdd18f13a49c20acc392ac5
viveliduc <viveliduc2@gmail.com>
2023-03-28 16:46:03 -0600

    GUI
    GUI inicial con un par de comandos pa conectar con el arduino

---
+++ gui MATLAB/gui1.m
@@ -0,0 +1,178 @@
+function varargout = gui1(varargin)
+% GUI1 MATLAB code for gui1.fig
+%      GUI1, by itself, creates a new GUI1 or raises the existing
+%      singleton*.
+%
+%      H = GUI1 returns the handle to a new GUI1 or the handle to
+%      the existing singleton*.
+%
+%      GUI1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI1.M with the given input arguments.
+%
+%      GUI1('Property','Value',...) creates a new GUI1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui1
+
+% Last Modified by GUIDE v2.5 28-Mar-2023 16:21:53
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui1_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+%Saludito
+%fprintf('Ready to connect! :D \n');
+
+
+% --- Executes just before gui1 is made visible.
+function gui1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui1 (see VARARGIN)
+
+% Choose default command line output for gui1
+handles.output = hObject;
+set(handles.text4,'string','SUP -Vivi');
+pause(1.0);
+set(handles.text4,'string','HOLI UWU -Polo');
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+%ARRIBA
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+fprintf('Arriba\n'); %Aqui se sumaria a la variable que lleva los grados de movimiento (0-300)
+
+%ABAJO
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf('Abajo\n');
+
+%IZQUIERDA
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf('Izquierda\n');
+
+%DERECHA
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fprintf('Derecha\n');
+
+
+% --- Executes on button press in togglebutton1.
+function togglebutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+%Tomando el valor del boton
+%Tomando el valor del boton
+valor_connect = get(handles.togglebutton1,'value');
+
+if valor_connect == 1
+    %Generando la conexion al arduino
+    %connect_ar = arduino('COM4'); %Checar el puerto al que se conecta el arduino
+    set(handles.togglebutton1,'BackgroundColor','green');
+    set(handles.togglebutton1,'string','CONNECTED');
+    set(handles.togglebutton1,'BackgroundColor','green');
+    fprintf('Connected \n');  
+else
+    %delete(instrfind({'Port'},{'COM4'}));
+    set(handles.togglebutton1,'BackgroundColor','red');
+    set(handles.togglebutton1,'string','DISCONNECTED');
+    fprintf('Disconnected \n');
+    pause(1);
+    %Restableciendo el color del boton -- maybe crashee
+    set(handles.togglebutton1,'BackgroundColor',[0.929 0.694 0.125]);
+    set(handles.togglebutton1,'string','CONNECT');
+    fprintf('Ready to connect :)\n');
+
+end
+
+
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton1
+
+
+% --- Executes during object creation, after setting all properties.
+function axes1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes1
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+    set(hObject,'string','HOLI');
+end
